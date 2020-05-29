597475fdedeb8db28d51fece3bdfc9d73f336feb
Martin Folke <folkemartin.uk@gmail.com>
2020-05-29 21:05:15 +0100

    added responsive folding menu for small screens

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-window.onscroll = function() {scrollFunction()};
+//window.onscroll = function() {scrollFunction()};
 
-function scrollFunction() {
-  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    document.getElementById("nav_bar").style.grid-row = "1";
-  } else {
-    document.getElementById("nav_bar").style.grid-row = "2";
-  }
+
+//function scrollFunction() {
+// if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+//    document.getElementById("nav_bar").style.grid-row = "1";
+//  } else {
+//    document.getElementById("nav_bar").style.grid-row = "2";
+//  }
+//}
+
+function open_nav() {
+  document.getElementById("sideMenuId").style.width = "180px";
+}
+
+function close_nav() {
+  document.getElementById("sideMenuId").style.width = "0px";
 }
